370aceec395ee990dba8200a994b8c3789c825c6
lch <lch@odoo.com>
2020-06-16 14:46:01 +0530

    js-training-lch

---
+++ session2/controller/controller.js
@@ -0,0 +1,55 @@
+
+document.querySelector(".btn").addEventListener("click",(e) =>
+	{
+		e.preventDefault();
+		var text_box = document.querySelector('#text_box').value;
+		var m= new Model();
+		var data=m.Save_Data(text_box);
+		var tbody=  document.querySelector(".table #tbody");
+		tbody.innerHTML='';
+
+		create_table(data);
+
+
+
+	});
+
+
+function create_table(data) {
+
+	Object.keys(data).forEach(function(element){
+
+
+			var tr= document.createElement("tr");
+			var td = document.createElement("td");
+			var td2 = document.createElement("td");
+			var btn_html =" <button type='submit' id='"+element+"' class='btn Delete btn-success btn-sm btn-default'>Delete</button>";
+			
+			td2.addEventListener('click',(e) => {
+			
+
+				if (e.srcElement.type =='submit')
+				{
+						var btn=e.srcElement;
+						var uuid=btn.id;
+						var m= new Model();
+
+						data=m.delete_data(uuid);
+						tbody.innerHTML='';
+						create_table(data);
+
+						
+
+
+				}
+			});
+
+			td2.innerHTML=btn_html;
+			td.append(data[element]);
+			tr.append(td);
+			tr.append(td2);
+			tbody.append(tr);
+
+			});
+		
+}
---
+++ session2/model/model.js
@@ -0,0 +1,44 @@
+class Model{
+
+	Save_Data(text_box)
+	{
+		var uuid= create_UUID();
+		localStorage.setItem(uuid, text_box);
+		var data=allStorage()
+		return data;
+	}
+
+	delete_data(uuid)
+	{	
+		localStorage.removeItem(uuid);
+		var data=allStorage()
+		return data;
+	}
+
+}
+
+
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
+
+
+function allStorage() {
+
+    var archive = {}, 
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+    while ( i-- ) {
+        archive[ keys[i] ] = localStorage.getItem( keys[i] );
+    }
+
+    return archive;
+}
