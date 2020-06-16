3b102797c9a3090582e54e15d31682f3fd6e743d
Neel <nem@odoo.com>
2020-06-16 13:03:28 +0530

    [IMP] Validation included

--- Session 2/event.js
+++ Session 2/event.js
@@ -1,11 +1,17 @@
 document.getElementById('myForm').addEventListener('submit', function(ev){
 	ev.preventDefault();
 	const name = this.querySelector('input').value;
-	const m1 = new Model();
-	m1.model(name);
-	this.querySelector('input').value = "";
+	if (name=="") {
+		alert("Cannot be Empty");
+	} else{
+		const m1 = new Model();
+		m1.model(name);
+		this.querySelector('input').value = "";
+	}
+	
 });
 
 document.getElementById("clearBtn").addEventListener("click", function(ev){
 	localStorage.clear();
+	location.reload();
 });
