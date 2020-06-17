71646ca91d67a0a6bfefd522792a455816f72761
Nisha Patel <nit@odoo.com>
2020-06-17 11:14:49 +0530

    [ADD] session2: Delete Button

--- session_2/localstorage.js
+++ session_2/localstorage.js
@@ -1,27 +1,29 @@
 class Model{
 	model(name){
 		const id = Math.random().toString(36).substr(2, 9);
-		const dataSet = {id: id, name: name};
+		let dataSet = {id: id, name: name};
 		const localStorageDataSet = JSON.parse(localStorage.getItem('dataSet')) || [];
 		localStorageDataSet.push(dataSet);
 		localStorage.setItem('dataSet', JSON.stringify(localStorageDataSet));
-		console.log(localStorageDataSet);
 		const tableBody = document.querySelector('table tbody');
 		tableBody.innerHTML = "";
 		localStorageDataSet.forEach((value) => {
-			const tr = document.createElement('tr');
-			const td = document.createElement('td');
-			const btndel = document.createElement('button');
-			td.textContent = value.name;
-			btndel.innerHTML = "Delete";
-			tr.append(td);
-			tr.append(td, btndel);
-			tableBody.append(tr);
-			btndel.addEventListener("click", (ev) => {
-				ev.preventDefault();
-				tr.remove();
-				// window.localStorage.clear();
-			})
-		})
+		   const tr = document.createElement('tr');
+		   const td = document.createElement('td');
+		   const button = document.createElement('button');
+		   button.setAttribute('id', value.id);
+		   button.innerHTML = "Delete"
+		   td.textContent = value.name;
+		   tr.append(td, button);
+		   tableBody.append(tr);
+		   button.addEventListener('click', (ev) => {
+		       ev.preventDefault();
+		       dataSet = JSON.parse(localStorage.getItem('dataSet')) || [];
+		       let key = button.id;
+		       dataSet = dataSet.filter((record) => record.id !== key);
+		       localStorage.setItem('dataSet', JSON.stringify(dataSet));
+		       tr.remove();
+	       })
+	   })
 	}
 }
