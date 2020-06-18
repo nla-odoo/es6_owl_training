9ff16d20488bc38bd3aa3f073cf57ed5d4951349
Dhwani Patel <dwa@odoo.com>
2020-06-18 16:45:36 +0530

    [session 3] : widget

--- session_3/test_widget/models/widget.py
+++ session_3/test_widget/models/widget.py
@@ -3,5 +3,5 @@ from odoo import models, fields
 class widgetfield(models.Model):
 	_name = 'widget.field'
 
-	name = fields.char(string="Employee name")
+	name = fields.Char(string="Employee name")
 	salary = fields.Integer(string="Salary")
