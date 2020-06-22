12b323c2e53fa1449bdd3937bd602450dc48eb3d
Nisha Patel <nit@odoo.com>
2020-06-22 17:28:27 +0530

    [ADD] session_4: counter_app

---
+++ session_4/demo/app/app.js
@@ -0,0 +1,46 @@
+
+function app() {
+  const { useState } = owl.hooks;
+  const {Component, tags} = owl;
+  const { xml } = tags;
+
+  // Main root component
+  class App extends Component {
+      constructor() {
+          super(...arguments);
+          // simple state hook (reactive object)
+          this.counter = useState({ value: 0 });
+      }
+
+      increment(counter) {
+        this.counter.value++;
+      }
+
+      decrement(counter) {
+        this.counter.value--;
+      }
+  }
+  // Application setup
+  const app = new App();
+  app.mount(document.body);
+}
+
+/**
+ * Initialization code
+ * This code load templates, and make sure everything is properly connected.
+ */
+async function start() {
+  let templates;
+  try {
+    templates = await owl.utils.loadFile('app.xml');
+  } catch(e) {
+    console.error(`This app requires a static server.  If you have python installed, try 'python app.py'`);
+    return;
+  }
+  const env =  { qweb: new owl.QWeb({templates})};
+  owl.Component.env = env;
+  await owl.utils.whenReady();
+  app();
+}
+
+start();
